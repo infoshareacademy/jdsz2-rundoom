db0fa06bd4a3508516b7179095ad95fd0c6cbc3f
Bartłomiej Gruchała <43046270+bgruchala@users.noreply.github.com>
2018-10-21 10:17:28 +0200

    Added jdsz2ran-6_1_analiza-wg-dni-tygodnia.sql

---
+++ JDSZ2RAN-6/jdsz2ran-6_1_analiza-wg-dni-tygodnia.sql
@@ -0,0 +1,11 @@
+-- Ilość wniosków wg dni tygodnia z podziałem na lata
+
+SELECT date_part('dow', data_utworzenia) AS dzien_tygodnia,
+       COUNT(CASE WHEN date_part('year', data_utworzenia) = 2013 THEN id END) AS rok_2013,
+       COUNT(CASE WHEN date_part('year', data_utworzenia) = 2014 THEN id END) AS rok_2014,
+       COUNT(CASE WHEN date_part('year', data_utworzenia) = 2015 THEN id END) AS rok_2015,
+       COUNT(CASE WHEN date_part('year', data_utworzenia) = 2016 THEN id END) AS rok_2016,
+       COUNT(CASE WHEN date_part('year', data_utworzenia) = 2017 THEN id END) AS rok_2017,
+       COUNT(1) AS razem
+FROM wnioski
+GROUP BY dzien_tygodnia;
