060a4c7c8a7f4b9d1a30c74d078dfe3da510df25
Bartłomiej Gruchała <43046270+bgruchala@users.noreply.github.com>
2018-10-21 10:58:01 +0200

    Added jdsz2ran-6_3_analiza-wg-kwartalow.sql

---
+++ JDSZ2RAN-6/jdsz2ran-6_3_analiza-wg-kwartalow.sql
@@ -0,0 +1,11 @@
+-- Ilość wniosków wg kwartałów z podziałem na lata
+
+SELECT date_part('quarter', data_utworzenia) AS kwartal,
+       COUNT(CASE WHEN date_part('year', data_utworzenia) = 2013 THEN id END) AS rok_2013,
+       COUNT(CASE WHEN date_part('year', data_utworzenia) = 2014 THEN id END) AS rok_2014,
+       COUNT(CASE WHEN date_part('year', data_utworzenia) = 2015 THEN id END) AS rok_2015,
+       COUNT(CASE WHEN date_part('year', data_utworzenia) = 2016 THEN id END) AS rok_2016,
+       COUNT(CASE WHEN date_part('year', data_utworzenia) = 2017 THEN id END) AS rok_2017,
+       COUNT(1) AS razem
+FROM wnioski
+GROUP BY kwartal;
