e965f354983c1964027bcdb86d70ec9334c4fa07
Michal Chylinski <mic.chyl@gmail.com>
2018-10-21 09:56:46 +0200

    added first query for JDSZ2RAN-8

---
+++ JDSZ2RAN-8/JDSZ2RAN-8_1_sredni_czas_rospatrywania_wniosku_latach.sql
@@ -0,0 +1,7 @@
+/*Zapytanie pokazuje srednie czasy rozpatrywania wnioskow w latach*/
+select date_part('year', w.data_utworzenia) as rok, avg(greatest(aw.data_zakonczenia, w.data_utworzenia)-least(aw.data_zakonczenia, w.data_utworzenia)) as sredni_czas_obslugi_wniosku
+from wnioski w inner join analizy_wnioskow aw on w.id = aw.id_wniosku
+where 1=1
+    and aw.status like 'zaa%'
+group by date_part('year', w.data_utworzenia)
+order by 1,2;
