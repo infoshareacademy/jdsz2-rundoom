141e8a63d7a8b58b7092f3866f35a16d7588eb45
Bartłomiej Gruchała <43046270+bgruchala@users.noreply.github.com>
2018-10-29 20:10:25 +0100

    Files related to the analysis and visualization of the number of applications on particular days of the week have been added and modified

--- JDSZ2RAN-6/jdsz2ran-6_1_analiza-wg-dni-tygodnia.sql
+++ JDSZ2RAN-6/jdsz2ran-6_1_analiza-wg-dni-tygodnia.sql
@@ -1,11 +1,38 @@
 -- Ilość wniosków wg dni tygodnia z podziałem na lata
+-- Rok 2013 odrzucono, ze względu na małą istotność
+-- Rok 2018 odrzucono, ze względu na, że zawierał dane tylko z 2 pierwszych miesiecy
 
 SELECT date_part('dow', data_utworzenia) AS dzien_tygodnia,
-       COUNT(CASE WHEN date_part('year', data_utworzenia) = 2013 THEN id END) AS rok_2013,
+       COUNT(CASE WHEN date_part('year', data_utworzenia) = 2014 THEN id END) AS rok_2014,
+       COUNT(CASE WHEN date_part('year', data_utworzenia) = 2015 THEN id END) AS rok_2015,
+       COUNT(CASE WHEN date_part('year', data_utworzenia) = 2016 THEN id END) AS rok_2016,
+       COUNT(CASE WHEN date_part('year', data_utworzenia) = 2017 THEN id END) AS rok_2017
+FROM wnioski
+GROUP BY dzien_tygodnia;
+
+-- Dni tygodnia słownie
+
+SELECT to_char(data_utworzenia, 'day') AS dzien_tygodnia,
+       COUNT(CASE WHEN date_part('year', data_utworzenia) = 2014 THEN id END) AS rok_2014,
+       COUNT(CASE WHEN date_part('year', data_utworzenia) = 2015 THEN id END) AS rok_2015,
+       COUNT(CASE WHEN date_part('year', data_utworzenia) = 2016 THEN id END) AS rok_2016,
+       COUNT(CASE WHEN date_part('year', data_utworzenia) = 2017 THEN id END) AS rok_2017
+FROM wnioski
+GROUP BY dzien_tygodnia;
+
+-- Bardziej rozbudowana analityka
+
+WITH wnioski_dow as (
+SELECT date_part('dow', data_utworzenia) AS dzien_tygodnia,
        COUNT(CASE WHEN date_part('year', data_utworzenia) = 2014 THEN id END) AS rok_2014,
        COUNT(CASE WHEN date_part('year', data_utworzenia) = 2015 THEN id END) AS rok_2015,
        COUNT(CASE WHEN date_part('year', data_utworzenia) = 2016 THEN id END) AS rok_2016,
        COUNT(CASE WHEN date_part('year', data_utworzenia) = 2017 THEN id END) AS rok_2017,
-       COUNT(1) AS razem
+       COUNT(CASE WHEN date_part('year', data_utworzenia) BETWEEN 2014 AND 2017 THEN id END) AS razem
 FROM wnioski
-GROUP BY dzien_tygodnia;
+GROUP BY dzien_tygodnia)
+
+SELECT *,
+round(avg(razem) over(), 0) as srednia_razem,
+round(stddev_pop(razem) over(), 0) as odch_std
+FROM wnioski_dow;
