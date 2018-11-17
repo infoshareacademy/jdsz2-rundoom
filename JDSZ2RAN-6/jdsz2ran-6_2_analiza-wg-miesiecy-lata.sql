71a1d2fd43a59e8da66ec66860a9d5d0788a1a00
Bartłomiej Gruchała <43046270+bgruchala@users.noreply.github.com>
2018-11-17 22:12:12 +0100

    Added and modified files in the JDSZ2RAN-6 folder

--- JDSZ2RAN-6/jdsz2ran-6_2_analiza-wg-miesiecy-lata.sql
+++ JDSZ2RAN-6/jdsz2ran-6_2_analiza-wg-miesiecy-lata.sql
@@ -1,11 +1,38 @@
--- Ilość wniosków wg dni miesiąca z podziałem na lata
+-- Ilość wniosków wg miesięcy z podziałem na lata
+-- Rok 2013 odrzucono, ze względu na małą istotność
+-- Rok 2018 odrzucono, ze względu na to, że zawierał dane tylko z 2 pierwszych miesiecy
 
 SELECT date_part('month', data_utworzenia) AS miesiac,
-       COUNT(CASE WHEN date_part('year', data_utworzenia) = 2013 THEN id END) AS rok_2013,
+       COUNT(CASE WHEN date_part('year', data_utworzenia) = 2014 THEN id END) AS rok_2014,
+       COUNT(CASE WHEN date_part('year', data_utworzenia) = 2015 THEN id END) AS rok_2015,
+       COUNT(CASE WHEN date_part('year', data_utworzenia) = 2016 THEN id END) AS rok_2016,
+       COUNT(CASE WHEN date_part('year', data_utworzenia) = 2017 THEN id END) AS rok_2017
+FROM wnioski
+GROUP BY miesiac;
+
+-- Miesiące słownie
+
+SELECT to_char(data_utworzenia, 'month') AS miesiac,
+       COUNT(CASE WHEN date_part('year', data_utworzenia) = 2014 THEN id END) AS rok_2014,
+       COUNT(CASE WHEN date_part('year', data_utworzenia) = 2015 THEN id END) AS rok_2015,
+       COUNT(CASE WHEN date_part('year', data_utworzenia) = 2016 THEN id END) AS rok_2016,
+       COUNT(CASE WHEN date_part('year', data_utworzenia) = 2017 THEN id END) AS rok_2017
+FROM wnioski
+GROUP BY miesiac;
+
+-- Bardziej rozbudowana analityka
+
+WITH wnioski_month as (
+SELECT date_part('month', data_utworzenia) AS month,
        COUNT(CASE WHEN date_part('year', data_utworzenia) = 2014 THEN id END) AS rok_2014,
        COUNT(CASE WHEN date_part('year', data_utworzenia) = 2015 THEN id END) AS rok_2015,
        COUNT(CASE WHEN date_part('year', data_utworzenia) = 2016 THEN id END) AS rok_2016,
        COUNT(CASE WHEN date_part('year', data_utworzenia) = 2017 THEN id END) AS rok_2017,
-       COUNT(1) AS razem
+       COUNT(CASE WHEN date_part('year', data_utworzenia) BETWEEN 2014 AND 2017 THEN id END) AS razem
 FROM wnioski
-GROUP BY miesiac;
+GROUP BY month)
+
+SELECT *,
+round(avg(razem) over(), 0) as srednia_razem,
+round(stddev_pop(razem) over(), 0) as odch_std
+FROM wnioski_month;
