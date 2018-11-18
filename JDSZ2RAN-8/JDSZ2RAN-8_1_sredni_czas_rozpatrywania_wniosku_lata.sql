1a48c35ca211304a7b8f1714fa344934f767d561
Michal Chylinski <mic.chyl@gmail.com>
2018-11-18 13:44:57 +0100

    average time displayed in days

--- JDSZ2RAN-8/JDSZ2RAN-8_1_sredni_czas_rozpatrywania_wniosku_lata.sql
+++ JDSZ2RAN-8/JDSZ2RAN-8_1_sredni_czas_rozpatrywania_wniosku_lata.sql
@@ -1,5 +1,7 @@
 /*Zapytanie pokazuje srednie czasy rozpatrywania wnioskow w latach*/
-select date_part('year', w.data_utworzenia) as rok, avg(greatest(aw.data_zakonczenia, w.data_utworzenia)-least(aw.data_zakonczenia, w.data_utworzenia)) as sredni_czas_obslugi_wniosku
+select date_part('year', w.data_utworzenia) as rok, 
+--avg(greatest(aw.data_zakonczenia, w.data_utworzenia)-least(aw.data_zakonczenia, w.data_utworzenia)) as sredni_czas_obslugi_wniosku
+EXTRACT(epoch FROM (avg(greatest(aw.data_zakonczenia, w.data_utworzenia)-least(aw.data_zakonczenia, w.data_utworzenia))) )/(60 * 60 * 24)as sredni_czas_obslugi_wniosku_w_dniach
 from wnioski w inner join analizy_wnioskow aw on w.id = aw.id_wniosku
 where 1=1
     and aw.status like 'zaa%'
--- JDSZ2RAN-8/JDSZ2RAN-8_1_sredni_czas_rozpatrywania_wniosku_miesiace.sql
+++ JDSZ2RAN-8/JDSZ2RAN-8_1_sredni_czas_rozpatrywania_wniosku_miesiace.sql
@@ -1,7 +1,8 @@
 /*Zapytanie pokazuje srednie czasy rozpatrywania wnioskow w podziale na lata/miesiace*/
 select date_part('year', w.data_utworzenia) as rok,
        date_part('month', w.data_utworzenia) as miesiac,
-       avg(greatest(aw.data_zakonczenia, w.data_utworzenia)-least(aw.data_zakonczenia, w.data_utworzenia)) as sredni_czas_obslugi_wniosku
+       --avg(greatest(aw.data_zakonczenia, w.data_utworzenia)-least(aw.data_zakonczenia, w.data_utworzenia)) as sredni_czas_obslugi_wniosku
+	EXTRACT(epoch FROM (avg(greatest(aw.data_zakonczenia, w.data_utworzenia)-least(aw.data_zakonczenia, w.data_utworzenia))) )/(60 * 60 * 24)as sredni_czas_obslugi_wniosku_w_dniach
 from wnioski w inner join analizy_wnioskow aw on w.id = aw.id_wniosku
 where 1=1
     and aw.status like 'zaa%'
