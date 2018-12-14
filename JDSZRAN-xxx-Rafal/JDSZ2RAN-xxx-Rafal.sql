2b17e149301bfe61f4768506771730fea728dbb5
Rafal Kociecki <rafal.kociecki@gmail.com>
2018-12-14 15:01:53 +0100

    do prezentacji

--- JDSZRAN-xxx-Rafal/JDSZ2RAN-xxx-Rafal.sql
+++ JDSZRAN-xxx-Rafal/JDSZ2RAN-xxx-Rafal.sql
@@ -17,11 +17,17 @@ left join wnioski w on r.id_wniosku = w.id
 where date_part('year',w.data_utworzenia) between 2014 and 2017
 group by 1)
 
-Select yyyy_mm,ilosc_wnioskow_zlozonych,suma_rek_zlozonych,ilosc_wyplaconych,suma_rek,abs(suma_rek_zlozonych - suma_rek) as roznica,
+Select yyyy_mm,ilosc_wnioskow_zlozonych,round(suma_rek_zlozonych/ilosc_wnioskow_zlozonych::numeric,2) as sr_kw_per_wn,
+ilosc_wyplaconych,round(suma_rek/ilosc_wnioskow_zlozonych::numeric,2) as srednia_kw_wypl,
 round(ilosc_wyplaconych/ilosc_wnioskow_zlozonych::numeric,2) as procent_wyplaconych
 from DS
 left join w_rek on DS.yyyy_mm = w_rek.concat
 
+
+-- Select yyyy_mm,ilosc_wnioskow_zlozonych,suma_rek_zlozonych,ilosc_wyplaconych,suma_rek,abs(suma_rek_zlozonych - suma_rek) as roznica,
+-- round(ilosc_wyplaconych/ilosc_wnioskow_zlozonych::numeric,2) as procent_wyplaconych
+-- from DS
+-- left join w_rek on DS.yyyy_mm = w_rek.concat
 
 
 /*
@@ -30,6 +36,9 @@ from DS
 left join w_rek on DS.concat = w_rek.concat*/
 
 
+
+Select * from wnioski
+where date_part('year', data_utworzenia) = 2016 and date_part('month',data_utworzenia) = 3
 
 
 
